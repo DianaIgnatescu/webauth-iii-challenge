ab7b2d26de0cb91d6129002b0aa86fc7c600af68
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-03 13:13:29 +0100

    MVP: Fix indentation & reduce times password is hashed

--- index.js
+++ index.js
@@ -24,21 +24,21 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   const { username, password, department } = req.body;
   let user = req.body;
-  const hashedPw = bcrypt.hashSync(user.password, 15);
+  const hashedPw = bcrypt.hashSync(user.password, 12);
   user.password = hashedPw;
   if (!username || !password || !department) {
     res.status(400).json({ errorMessage: 'Missing username, or password, or department.' })
   } else {
     db('users').insert(user)
-        .then(arrayOfIds => {
-          return db('users').where({ id: arrayOfIds[0] });
-        })
-        .then(arrayOfUsers => {
-          res.status(201).json(arrayOfUsers[0])
-        })
-        .catch((error) => {
-          res.status(500).json({ errorMessage: 'The user could not be created.' });
-        })
+      .then(arrayOfIds => {
+        return db('users').where({ id: arrayOfIds[0] });
+      })
+      .then(arrayOfUsers => {
+        res.status(201).json(arrayOfUsers[0])
+      })
+      .catch((error) => {
+        res.status(500).json({ errorMessage: 'The user could not be created.' });
+      })
   }
 });
 
