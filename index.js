f78d64ba2fc482c1d1ffb1ca2e314794e302111d
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-03 12:51:49 +0100

    MVP: Create endpoint to register a user with hashed password

--- index.js
+++ index.js
@@ -19,6 +19,27 @@ server.get('/', (req, res) => {
   res.send('I\'m ready to partayy!');
 });
 
+server.post('/api/register', (req, res) => {
+  const { username, password, department } = req.body;
+  let user = req.body;
+  const hashedPw = bcrypt.hashSync(user.password, 15);
+  user.password = hashedPw;
+  if (!username || !password || !department) {
+    res.status(400).json({ errorMessage: 'Missing username, or password, or department.' })
+  } else {
+    db('users').insert(user)
+        .then(arrayOfIds => {
+          return db('users').where({ id: arrayOfIds[0] });
+        })
+        .then(arrayOfUsers => {
+          res.status(201).json(arrayOfUsers[0])
+        })
+        .catch((error) => {
+          res.status(500).json({ errorMessage: 'The user could not be created.' });
+        })
+  }
+});
+
 
 const port = 5000;
 server.listen(port, () => console.log(`\n*** Listening on http://localhost: ${port}! ***\n`));
